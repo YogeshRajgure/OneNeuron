4eacae8dab9947797a3cc5a48c0f64f2c90c0257
YogeshRajgure <noobcoderyogesh.vraj@gmail.com>
2021-09-29 01:56:40 +0530

    new organised code

--- or.py
+++ or.py
@@ -24,15 +24,15 @@ def main(data, eta, epochs, filename):
 
 if __name__ == '__main__': #<< entry point
 
-    AND = {
+    OR = {
             "x1" : [0,0,1,1],
             "x2" : [0,1,0,1],
-            "y"  : [0,0,0,1],
+            "y"  : [0,1,1,1],
         }
     
     ETA = 0.3 # 0 and 1
     EPOCHS = 10
 
-    main(data=AND, eta=ETA, epochs=EPOCHS, filename="and")
+    main(data=OR, eta=ETA, epochs=EPOCHS, filename="or")
 
 
